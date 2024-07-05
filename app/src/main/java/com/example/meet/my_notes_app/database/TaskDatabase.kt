b90d5dff0873fe6e62e4d0df4565b773f5da5ffc
aashiya368 meme <aashiya368@gmail.com>
2024-07-06 00:51:42 +0530

    First Commit.

---
+++ app/src/main/java/com/example/meet/my_notes_app/database/TaskDatabase.kt
@@ -0,0 +1,39 @@
+package com.example.meet.my_notes_app.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.meet.my_notes_app.converters.TypeConverter
+import com.example.meet.my_notes_app.dao.TaskDao
+import com.example.meet.my_notes_app.models.Task
+
+@Database(
+    entities = [Task::class],
+    version = 1,
+    exportSchema = false
+)
+@TypeConverters(TypeConverter::class)
+abstract class TaskDatabase : RoomDatabase() {
+
+    abstract val taskDao : TaskDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: TaskDatabase? = null
+        fun getInstance(context: Context): TaskDatabase {
+            synchronized(this) {
+                return INSTANCE ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    TaskDatabase::class.java,
+                    "task_db"
+                ).build().also {
+                    INSTANCE = it
+                }
+            }
+
+        }
+    }
+
+}
